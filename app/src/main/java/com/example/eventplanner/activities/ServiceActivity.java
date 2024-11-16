669b392fefb512cac52ca8408c95e554143379c3
Mina Radenković <mina.radenkovic@gmail.com>
2024-11-16 19:34:10 +0100

    feature-Mina: finished view of services for SPP

--- app/src/main/java/com/example/eventplanner/activities/ServiceActivity.java
+++ app/src/main/java/com/example/eventplanner/activities/ServiceActivity.java
@@ -1,8 +1,14 @@
 package com.example.eventplanner.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +18,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.eventplanner.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.slider.RangeSlider;
+
+import java.util.List;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -25,11 +36,23 @@ public class ServiceActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnFilters = findViewById(R.id.filterBtn);
+        btnFilters.setOnClickListener(v -> {
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this, R.style.FullScreenBottomSheetDialog);
+            View dialogView = getLayoutInflater().inflate(R.layout.service_filter, null);
+            bottomSheetDialog.setContentView(dialogView);
+            bottomSheetDialog.show();
+        });
+
+        FloatingActionButton btnAddNew = findViewById(R.id.addNewService);
+        btnAddNew.setOnClickListener(v -> {
+            Intent intent = new Intent(ServiceActivity.this, AddServiceActivity.class);
+            startActivity(intent);
+        });
     }
     public void editService(View view) {
-        Toast.makeText(this, "Card clicked", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(ServiceActivity.this, EditServiceActivity.class);
         startActivity(intent);
-
     }
 }
