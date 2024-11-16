418fe7806f07e2f32b61e294016da11006bfeaee
Mina Radenković <mina.radenkovic@gmail.com>
2024-11-16 03:29:59 +0100

    feature-Mina: added activity for SPP so he can view all his services

---
+++ app/src/main/java/com/example/eventplanner/activities/ServiceActivity.java
@@ -0,0 +1,35 @@
+package com.example.eventplanner.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.eventplanner.R;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_service_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void editService(View view) {
+        Toast.makeText(this, "Card clicked", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(ServiceActivity.this, EditServiceActivity.class);
+        startActivity(intent);
+
+    }
+}
